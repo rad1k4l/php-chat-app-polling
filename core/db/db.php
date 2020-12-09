b9dcd943561ef796b7b1ff3e7312949dd017a507
rad1k4l <zeynalli169@gmail.com>
2020-12-09 17:39:57 +0400

    cleanup bugfix

--- core/db/db.php
+++ core/db/db.php
@@ -1,45 +1,56 @@
 <?php
 
 namespace core\db;
+
 use PDO;
-class db{
+
+class db
+{
     public $conn;
     public $pod;
-    public function __construct(){
-        $this->connect('host' , 'database' , 'user' , 'password');
+
+    public function __construct()
+    {
+        $this->connect('host', 'database', 'user', 'password');
     }
-    public function connect($host, $db , $user , $pass ){
+
+    public function connect($host, $db, $user, $pass)
+    {
         //  include_once 'reyestr/database.php';
-        $dsn = 'mysql:host=' . $host . ';dbname=' . $db .';charset=utf8';
-        $user = $user;
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8';
         $password = $pass;
-        $this->conn = new \PDO($dsn, $user, $password , [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $this->conn = new \PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         return $this->conn;
     }
-    public function execute_fetch($sql , $data){
+
+    public function execute_fetch($sql, $data)
+    {
         $this->pod = $this->conn->prepare($sql);
         $this->pod->execute($data);
         return $this->pod->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function execute($sql , $data){
+    public function execute($sql, $data)
+    {
         $this->pod = $this->conn->prepare($sql);
         return $this->pod->execute($data);
 
     }
 
-    public function query($sql){
+    public function query($sql)
+    {
         $pod = $this->conn->prepare($sql);
 
         $pod->execute();
 
         $result = $pod->fetchall(PDO::FETCH_ASSOC);
 
-        if($result === false ){
+        if ($result === false) {
             return false;
-        }else{
+        } else {
             return $result;
         }
     }
 }
+
 ?>
